dade0ba8813d33ac778689fdc9238ae0f7a3ff35
GianCHerrera <83032013+GianCHerrera@users.noreply.github.com>
2022-02-23 09:43:13 -0500

    Ultimo cambio
    Sustentación PI

--- api/src/routes/dogs.js
+++ api/src/routes/dogs.js
@@ -10,7 +10,7 @@ const router = Router();
 
 router.get('/', (req, res, next) => {
     const { name } = req.query;
-    let dogsPromiseApi = axios.get('https://api.thedogapi.com/v1/breeds')
+    let dogsPromiseApi = axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`)
     let dogsPromiseDb = Razas.findAll({
         include: Temperamentos
     })
@@ -24,115 +24,173 @@ router.get('/', (req, res, next) => {
             let dogsApiCorrectInfo = dogsApi.data.map((dog) => {
                 getPesoMaximo = (dog.weight.imperial).split(' - ')
                 getAlturaMaxima = (dog.height.imperial).split(' - ')
-                getAñosDeVida = (dog.life_span).split( ' years')
+                getAñosDeVida = (dog.life_span).split(' years')
                 return {
                     id: dog.id,
                     nombre: dog.name,
                     altura: dog.height,
-                    pesoMaximo: parseFloat(getPesoMaximo[getPesoMaximo.length-1]),
+                    pesoMaximo: parseFloat(getPesoMaximo[getPesoMaximo.length - 1]),
                     pesoMinimo: parseFloat(getPesoMaximo[0]),
-                    alturaMaxima: parseFloat(getAlturaMaxima[getAlturaMaxima.length-1]),
+                    alturaMaxima: parseFloat(getAlturaMaxima[getAlturaMaxima.length - 1]),
                     alturaMinima: parseFloat(getAlturaMaxima[0]),
                     añosDeVida: getAñosDeVida[0],
                     imagen: dog.image.url,
-                    temperamentos:dog.temperament
+                    temperamentos: dog.temperament
                 }
             })
-
-            // dogsDb.map(dog =>{
-            //     if(dog.dataValues.temperaments.length!==0){
-            //         let str = '';
-            //         dog.dataValues.temperaments.map(temp=>{
-            //             str = str + temp.dataValues.nombre + ', '
-            //         })
-            //         dog.dataValues.temperaments = str;
-            //     }
-            // })
-            
-            // console.log(dogsDb[0].dataValues.temperamentos.dataValues.nombre)
-             let allDogs = [...dogsApiCorrectInfo, ...dogsDb]
+            let allDogs = [...dogsApiCorrectInfo, ...dogsDb]
             if (name) {
                 // Si hay un name en query, lo busca tanto en la api con en la db
                 let searchResult = [];
 
-                for(let i=0; i<allDogs.length; i++){
+                for (let i = 0; i < allDogs.length; i++) {
                     let str = String(allDogs[i].nombre).toLowerCase()
-                    if(str.includes(name)){
+                    if (str.includes(name)) {
                         searchResult.push(allDogs[i])
                     }
                 }
-                if(searchResult.length === 0 ){
+                if (searchResult.length === 0) {
                     res.status(404).send('No hay razas con ese nombre')
-                }else{
+                } else {
                     res.send(searchResult)
                 }
-            }else{
-                    res.send(allDogs)
+            } else {
+                res.send(allDogs)
             }
         })
         .catch(error => next(error))
 })
 
-router.get('/apidogs', async  (req,res,next)=>{
-    const allDogs = await getApiDogs();
-    res.send(allDogs)
+router.get('/apidogs', async (req, res, next) => {
+    try {
+        const allDogs = await getApiDogs();
+        res.send(allDogs)
+    } catch (error) {
+        next(error)
+    }
 })
 
-router.get('/db', (req,res,next)=>{
+// router.get('/apidogs', (req,res,next)=>{
+//     return axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`)
+//     .then(respuesta=>{
+//         res.send(respuesta.data)
+//     })
+//     .catch(error=>next(error))
+// })
+
+
+
+router.get('/db', (req, res, next) => {
     return Razas.findAll({
         include: Temperamentos
     })
-    .then(respuesta=>{
-        res.send(respuesta)
-    }).catch(error=> next(error))
+        .then(respuesta => {
+            res.send(respuesta)
+        }).catch(error => next(error))
 })
 
-router.get('/:id',  async (req, res, next) => { 
+
+// router.get('/db', async (req, res, next) => {
+//     try{
+//         let dogsDB = await Razas.findAll({
+//                 include: Temperamentos
+//             }
+//         )
+//         res.send(dogsDB)
+//     }catch(error){
+//         next(error)
+//     }
+// })
+
+router.get('/:id', async (req, res, next) => {
     let { id } = req.params;
 
-    
-    if (typeof id === 'string' && id.length < 10) {
-        id=parseInt(id)
-        let dogsApi =  await axios.get('https://api.thedogapi.com/v1/breeds/');
-        let resApi = {};
-        dogsApi.data.map(dog =>{
 
-            getPesoMaximo = (dog.weight.imperial).split(' - ')
+    if (typeof id === 'string' && id.length < 10) {
+        try {
+            id = parseInt(id)
+            let dogsApi = await axios.get('https://api.thedogapi.com/v1/breeds/');
+            let resApi = {};
+            dogsApi.data.map(dog => {
+
+                getPesoMaximo = (dog.weight.imperial).split(' - ')
                 getAlturaMaxima = (dog.height.imperial).split(' - ')
-                getAñosDeVida = (dog.life_span).split( ' years')
-            if(dog.id === id){
-                resApi = {
-                    id: dog.id,
-                    nombre: dog.name,
-                    altura: dog.height,
-                    pesoMaximo: parseFloat(getPesoMaximo[getPesoMaximo.length-1]),
-                    pesoMinimo: parseFloat(getPesoMaximo[0]),
-                    alturaMaxima: parseFloat(getAlturaMaxima[getAlturaMaxima.length-1]),
-                    alturaMinima: parseFloat(getAlturaMaxima[0]),
-                    añosDeVida: getAñosDeVida[0],
-                    imagen: dog.image.url,
-                    temperamentos:dog.temperament
-                }  
-            }
-        })
-        res.send(resApi)
+                getAñosDeVida = (dog.life_span).split(' years')
+                if (dog.id === id) {
+                    resApi = {
+                        id: dog.id,
+                        nombre: dog.name,
+                        altura: dog.height,
+                        pesoMaximo: parseFloat(getPesoMaximo[getPesoMaximo.length - 1]),
+                        pesoMinimo: parseFloat(getPesoMaximo[0]),
+                        alturaMaxima: parseFloat(getAlturaMaxima[getAlturaMaxima.length - 1]),
+                        alturaMinima: parseFloat(getAlturaMaxima[0]),
+                        añosDeVida: getAñosDeVida[0],
+                        imagen: dog.image.url,
+                        temperamentos: dog.temperament
+                    }
+                }
+            })
+            res.send(resApi)
+        } catch (error) {
+            next(error)
+        }
     } else {
-        return resultadoBusqueda = Razas.findByPk(id)
-        .then((resultadoBusqueda) => {
+        try{
+            let resultadoBusqueda = await Razas.findByPk(id)
             res.send(resultadoBusqueda)
-        })
-        .catch((error) => next(error))
+        }catch(error){
+            next(error)
+        }
     }
-    
 })
+
+
+// router.get('/:id', (req, res, next) => {
+//     let id = req.params.id;
+
+//     if (typeof id === 'string' && id.length < 10) {
+//         id = parseInt(id)
+//         let resApi = {}
+//         return axios.get('https://api.thedogapi.com/v1/breeds/')
+//             .then(respuesta => {
+//                 respuesta.data.map(dog => {
+//                     getPesoMaximo = (dog.weight.imperial).split(' - ')
+//                     getAlturaMaxima = (dog.height.imperial).split(' - ')
+//                     getAñosDeVida = (dog.life_span).split(' years')
+//                     if (dog.id === id) {
+//                         resApi = {
+//                             id: dog.id,
+//                             nombre: dog.name,
+//                             altura: dog.height,
+//                             pesoMaximo: parseFloat(getPesoMaximo[getPesoMaximo.length - 1]),
+//                             pesoMinimo: parseFloat(getPesoMaximo[0]),
+//                             alturaMaxima: parseFloat(getAlturaMaxima[getAlturaMaxima.length - 1]),
+//                             alturaMinima: parseFloat(getAlturaMaxima[0]),
+//                             añosDeVida: getAñosDeVida[0],
+//                             imagen: dog.image.url,
+//                             temperamentos: dog.temperament
+//                         }
+//                     }
+//                 })
+//                 res.send(resApi)
+//             }).catch(error => next(error))
+//     } else {
+//         return resultadoBusqueda = Razas.findByPk(id)
+//             .then((resultadoBusqueda) => {
+//                 res.send(resultadoBusqueda)
+//             })
+//             .catch((error) => next(error))
+//     }
+// })
 
 
 
 
 router.post('/', (req, res, next) => {
-    const { nombre, imagen, alturaMaxima, pesoMaximo,alturaMinima, pesoMinimo,añosDeVida,temperamentos } = req.body;
+    const { nombre, imagen, alturaMaxima, pesoMaximo, alturaMinima, pesoMinimo, añosDeVida, temperamentos } = req.body;
 
-    if(nombre, alturaMaxima,pesoMaximo){
+    if (nombre, alturaMaxima, pesoMaximo) {
         return Razas.create({
             nombre,
             alturaMaxima,
@@ -143,49 +201,49 @@ router.post('/', (req, res, next) => {
             imagen,
             temperamentos
         })
-        .then((newRaza) => {
-            res.status(201).send(newRaza);
-        })
-        .catch(error => next(error))
-    }else{
+            .then((newRaza) => {
+                res.status(201).send(newRaza);
+            })
+            .catch(error => next(error))
+    } else {
         res.send('No hay suficiente información para agregar Raza')
     }
 
-    
+
 })
 
 router.post('/:dogsId/temperament/:temperamentId', (req, res, next) => {
     try {
         const { dogsId, temperamentId } = req.params;
-        mixinDogTemperament(dogsId,temperamentId)
+        mixinDogTemperament(dogsId, temperamentId)
         res.status(201)
     } catch (error) {
         next(error);
     }
 })
 
-async function mixinDogTemperament(dogsId, temperamentId){
-        const raza = await Razas.findByPk(dogsId);
-        await raza.addTemperaments(temperamentId); // mixin de sequelize, donde comienza por add y termina con el nombre de la tabla
+async function mixinDogTemperament(dogsId, temperamentId) {
+    const raza = await Razas.findByPk(dogsId);
+    await raza.addTemperaments(temperamentId); // mixin de sequelize, donde comienza por add y termina con el nombre de la tabla
 }
-async function getApiDogs (){
+async function getApiDogs() {
     const apiRequest = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`)
-    const apiDogs = await apiRequest.data.map( dog=>{
+    const apiDogs = await apiRequest.data.map(dog => {
 
         getPesoMaximo = (dog.weight.imperial).split(' - ')
         getAlturaMaxima = (dog.height.imperial).split(' - ')
-        getAñosDeVida = (dog.life_span).split( ' years')
-        return{
+        getAñosDeVida = (dog.life_span).split(' years')
+        return {
             id: dog.id,
-                    nombre: dog.name,
-                    altura: dog.height,
-                    pesoMaximo: parseFloat(getPesoMaximo[getPesoMaximo.length-1]),
-                    pesoMinimo: parseFloat(getPesoMaximo[0]),
-                    alturaMaxima: parseFloat(getAlturaMaxima[getAlturaMaxima.length-1]),
-                    alturaMinima: parseFloat(getAlturaMaxima[0]),
-                    añosDeVida: getAñosDeVida[0],
-                    imagen: dog.image.url,
-                    temperamentos:dog.temperament
+            nombre: dog.name,
+            altura: dog.height,
+            pesoMaximo: parseFloat(getPesoMaximo[getPesoMaximo.length - 1]),
+            pesoMinimo: parseFloat(getPesoMaximo[0]),
+            alturaMaxima: parseFloat(getAlturaMaxima[getAlturaMaxima.length - 1]),
+            alturaMinima: parseFloat(getAlturaMaxima[0]),
+            añosDeVida: getAñosDeVida[0],
+            imagen: dog.image.url,
+            temperamentos: dog.temperament
         }
     })
     return apiDogs;
@@ -193,61 +251,4 @@ async function getApiDogs (){
 
 
 
-// router.get('/',async (req,res,next)=>{
-    //     const dogs = await Razas.findAll({
-        //         include: Temperamento
-        //     });
-        //     res.send(dogs);
-        // })
-        
-        module.exports = router;
-        
-        // router.get('/', (req, res, next) => {
-        
-        //     const { name } = req.query;
-        //     let dogsPromiseApi;
-        //     let dogsPromiseDb;
-        
-        //     if (name) {
-        //         dogsPromiseApi = axios.get(`https://api.thedogapi.com/v1/breeds/search?q=${name}`)
-        //         dogsPromiseDb = Razas.findAll({
-        //             include: Temperamento,
-        //             where: {
-        //                 nombre: {
-        //                     [Op.iLike]: '%' + name + '%'
-        //                 }
-        //             }
-        //         })
-        //     } else {
-        //         dogsPromiseApi = axios.get('https://api.thedogapi.com/v1/breeds')
-        //         dogsPromiseDb = Razas.findAll({
-        //             include: Temperamento
-        //         })
-        //     }
-        //     Promise.all([
-        //         dogsPromiseApi,
-        //         dogsPromiseDb
-        //     ])
-        //         .then((respuesta) => {
-        //             const [dogsApi, dogsDb] = respuesta;
-        //             let dogsApiCorrectInfo = dogsApi.data.map((dog) => {
-        //                 console.log(dog.image.url);
-        //                 return {
-        //                     id: dog.id,
-        //                     nombre: dog.name,
-        //                     altura: dog.height,
-        //                     peso: dog.weight,
-        //                     añosDeVida: dog.life_span,
-        //                     imagen: dog.image.url
-        //                 }
-        //             })
-        //             let allDogs = [...dogsApiCorrectInfo, ...dogsDb]
-        //             if(allDogs.length===0){
-        //                 res.status(404).send('No hay razas con ese nombre')
-        //             }else{
-        //                 res.send(allDogs)
-        //             }
-        //         })
-        //         .catch(error => next(error))
-        
-        // })
+module.exports = router;
